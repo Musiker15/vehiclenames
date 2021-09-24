39e8607f39ecf28c1021065c669b4bb371788787
Musiker15 <info@musiker15.de>
2021-09-24 17:50:31 +0200

    Fixed some issues
    Now Script is working!

--- __resource.lua
+++ __resource.lua
@@ -1,8 +1,4 @@
-fx_version 'adamant'
-game 'gta5'
-description 'ESX Vehicle Names'
-Author 'Musiker15'
-version '1.0.0'
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 client_script {
     'vehicle_names.lua' 
--- vehicle_names.lua
+++ vehicle_names.lua
@@ -1,12 +1,34 @@
+function AddTextEntry(key, value)
+    Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+
 Citizen.CreateThread(function()
-  -- AddTextEntry("model", "Name") --> Copy this and Paste it in the next line. Wihtout an ,
+    -- AddTextEntry("model", "Name") --> Copy this and Paste it in the next line. Wihtout an ,
+  
+    -- Replace "model" with the spawnname of the Vehicle
+    -- Replace "Name" with the Name of the Vehicle
+  
+    -- Example: AddTextEntry("rs7", "Audi RS7")
 
-  -- Replace "model" with the spawnname of the Vehicle
-  -- Replace "Name" with the Name of the Vehicle
-
-  -- Example: AddTextEntry("rs7", "Audi RS7")
-
-  AddTextEntry("model", "Name")
-
-  -- AddTextEntry("model", "Name") --> Copy this and Paste it in the next line. Wihtout an ,
+    -- Ambulance
+    -- Police
+    -- Aston Martin
+    -- Audi
+    -- Bentley
+    -- BMW
+    -- Bugatti
+    -- Camaro
+    -- Corvette
+    -- Ferrari
+    -- Ford
+    -- Königsegg
+    -- Lamborghini
+    -- Lowrider
+    -- McLaren
+    -- Mercedes
+    -- Motoräder
+    -- Nissan
+    -- Porsche
+    -- Rolls Royce
+    -- Tesla
 end)
