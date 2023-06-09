6c82c6c13e65ba9fd88062007febb5672dcdf0fd
Musiker15 <info@musiker15.de>
2023-06-09 16:10:09 +0200

    Update vehicle_names.lua

--- vehicle_names.lua
+++ vehicle_names.lua
@@ -2,7 +2,7 @@ function AddTextEntry(key, value)
     Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
 end
 
-Citizen.CreateThread(function()
+CreateThread(function()
     -- Ambulance
     -- Police
     -- Aston Martin
